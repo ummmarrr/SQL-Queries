db08c57f54b474cb3d65cd40b905b15ec0e5acc5
Umar Momin <83031532+ummmarrr@users.noreply.github.com>
2025-07-02 11:50:17 +0530

    Add files via upload

---
+++ Meesho_HackerRank_SQL.sql
@@ -0,0 +1,20 @@
+select * from products
+select * from customer_budget;
+
+with adding_cost_cte as(
+	select *
+		,sum(cost)over(order by cost) costsum
+	from products
+)
+,cte as(
+	select cb.customer_id,acc.product_id
+	from customer_budget cb 
+	left join adding_cost_cte acc on cb.budget >= acc.costsum
+	group by cb.customer_id,acc.product_id
+)
+select customer_id,string_agg(product_id,'') Products_available
+from cte
+group by customer_id
+
+
+--M2: Optimized
